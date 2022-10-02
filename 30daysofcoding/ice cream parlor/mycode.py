9f24ce9635a7defa47571a0404efdb71f3f9bf4a
Gopika G <71868599+g0pika@users.noreply.github.com>
2022-10-02 14:14:55 +0530

    Create mycode.py
    Two friends like to pool their money and go to the ice cream parlor. They always choose two distinct flavors and they spend all of their money.
    
    Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have.

---
+++ 30daysofcoding/ice cream parlor/mycode.py
@@ -0,0 +1,37 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def icecreamParlor(m, arr):
+    for i in range(n):
+        for j in range(n):
+            if i != j and arr[i]+arr[j] == m:
+                return i+1, j+1
+              
+#This code will have a time complexity of O(n^2). 
+#So let's move on to the next approach using hashmap.               
+    
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
