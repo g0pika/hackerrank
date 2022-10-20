42f73c070cd23b59aadcf7c2d7b8b23c3b08e2af
Gopika G <71868599+g0pika@users.noreply.github.com>
2022-10-21 00:30:29 +0530

    Create uniqueelement.py
    Given an array of integers, where all elements but one occur twice, find the unique element.

---
+++ 30daysofcoding/uniqueelement.py
@@ -0,0 +1,29 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+def lonelyinteger(a):
+    a = sorted(a)
+    if len(a) <= 2:
+        return(a[0])
+    elif a[0] != a[1]:
+        return a[0]
+    else:
+        return lonelyinteger(a[2:])
+                    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
